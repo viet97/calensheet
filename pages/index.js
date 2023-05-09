ccc7511c452046ed2ce5b93b5f675cbc693bc9a1
VietDQ <quocvietdang97@gmail.com>
2023-05-09 17:08:15 +0700

    add google api

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import HelloWorld from '../components/hello-world'
-
+import gapi from "googleapis"
 export default function Home() {
+  const CLIENT_ID = "900185296667-ng4uaamvbe0p32891l8rrv7ne1lflheu.apps.googleusercontent.com"
+  const API_KEY = "AIzaSyAF1uyOdZpqJNNYzqW0EUoPiro00PBbtC0"
+  
+  const signin = ()=>{
+  }
+  
   return (
     <div className="app">
-      <HelloWorld />
+      <div
+      onClick={signin}>
+        sign in google sheet
+      </div>
     </div>
   )
 }
