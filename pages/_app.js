887848964a6aa23e2ec695c3644a8ab616af773e
VietDQ <quocvietdang97@gmail.com>
2023-05-11 16:02:23 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles.scss'
-
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,35 @@
-import HelloWorld from '../components/hello-world'
 // import gapi from "googleapis"
 import Script from 'next/script';
 import { useState } from 'react';
+import styles from "./index.module.scss";
+import { Input } from "antd"
+import DateTimePicker from 'react-datetime-picker';
+import { WithContext as ReactTags } from 'react-tag-input';
+
+const KeyCodes = {
+  comma: 188,
+  enter: 13,
+  space: 32,
+};
+const delimiters = [KeyCodes.comma, KeyCodes.enter, KeyCodes.space];
 
 export default function Home() {
   const [gisInited, setgisInited] = useState(false)
   const [gapiInited, setgapiInited] = useState(false)
-  const [tokenClient, setTokenClient] = useState(false)
+  const [tokenClient, setTokenClient] = useState()
+  const [title, setTitle] = useState("")
+  const [location, setLocation] = useState("")
+  const [description, setDescription] = useState("")
+  const [startTime, setStartTime] = useState(null)
+  const [endTime, setEndTime] = useState(null)
+  const [tags, setTags] = useState([]);
+
   const CLIENT_ID = "900185296667-dg1gdnl5rgtgch5hdtf5ns8aqhttvchu.apps.googleusercontent.com"
   const API_KEY = "AIzaSyAF1uyOdZpqJNNYzqW0EUoPiro00PBbtC0"
-  const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
-  const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
-
+  const DISCOVERY_DOC = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
+    'https://sheets.googleapis.com/$discovery/rest?version=v4'];
+  const SCOPES = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/spreadsheets.readonly';
+  // const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
   function gisLoaded() {
     console.log("google.accounts.oauth2423")
     const tokenClient = google.accounts.oauth2.initTokenClient({
@@ -58,6 +76,7 @@ export default function Home() {
       if (resp.error !== undefined) {
         throw (resp);
       }
+      console.log("response", google.accounts)
       await listMajors();
     };
 
@@ -74,21 +93,141 @@ export default function Home() {
   async function initializeGapiClient() {
     await gapi.client.init({
       apiKey: API_KEY,
-      discoveryDocs: [DISCOVERY_DOC],
+      discoveryDocs: DISCOVERY_DOC,
     });
     setgapiInited(true)
   }
 
-  return (
-    <>
-      <div className="app">
+  const handleDelete = i => {
+    setTags(tags.filter((tag, index) => index !== i));
+  };
+
+  const handleAddition = tag => {
+    setTags([...tags, tag]);
+  };
+
+  const handleDrag = (tag, currPos, newPos) => {
+    const newTags = tags.slice();
+
+    newTags.splice(currPos, 1);
+    newTags.splice(newPos, 0, tag);
+
+    // re-render
+    setTags(newTags);
+  };
+
+
+  const createEvent = () => {
+    const event = {
+      'summary': 'Google I/O 2015',
+      'location': '800 Howard St., San Francisco, CA 94103',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': '2023-05-11T09:00:00',
+        'timeZone': 'America/Los_Angeles'
+      },
+      'end': {
+        'dateTime': '2023-05-11T17:00:00',
+        'timeZone': 'America/Los_Angeles'
+      },
+      // 'recurrence': [
+      //   'RRULE:FREQ=DAILY;COUNT=2'
+      // ],
+      'attendees': [
+        { 'email': 'lpage@example.com' },
+        { 'email': 'sbrin@example.com' }
+      ],
+      'reminders': {
+        'useDefault': false,
+        'overrides': [
+          { 'method': 'email', 'minutes': 24 * 60 },
+          { 'method': 'popup', 'minutes': 10 }
+        ]
+      }
+    };
+
+    const request = gapi.client.calendar.events.insert({
+      'calendarId': 'primary',
+      'resource': event
+    });
+
+    request.execute(function (event) {
+    });
+  }
+
+  const onChangeEndTime = date => {
+    setEndTime(date)
+  }
+
+  const onChangeStartTime = date => {
+    setStartTime(date)
+  }
+
+  return (<>
+    <div className={styles.wrapper}>
+      <div
+        className={styles.container}>
+        {gisInited && gapiInited ? <div
+          onClick={handleAuthClick}
+          className={styles.loginGoogle}>
+          Login
+        </div> : null}
         <div
-          onClick={handleAuthClick}>
-          sign in google sheet
+          className={styles.email}>
+          User:
         </div>
+        <div
+          className={styles.title}>
+          Event Title :  <Input
+            className={styles.input}
+            placeholder="Enter Title"
+          // value={val}
+          // onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div
+          className={styles.title}>
+          Event Location :  <Input
+            className={styles.input}
+            placeholder="Enter Location"
+          // value={val}
+          // onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div
+          className={styles.title}>
+          Event Description :  <Input
+            className={styles.input}
+            placeholder="Enter Description"
+          // value={val}
+          // onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div
+          className={styles.title}>
+          Start Time :   <DateTimePicker onChange={onChangeStartTime} value={startTime} />
+        </div>
+        <div
+          className={styles.title}>
+          End Time : <DateTimePicker onChange={onChangeEndTime} value={endTime} />
+        </div>
+        <div
+          className={styles.title}>
+          Attendees :  <ReactTags
+            tags={tags}
+            suggestions={[]}
+            delimiters={delimiters}
+            handleDelete={handleDelete}
+            handleAddition={handleAddition}
+            handleDrag={handleDrag}
+            inputFieldPosition="bottom"
+            autocomplete
+          />
+        </div>
+
       </div>
-      <Script async defer src="https://apis.google.com/js/api.js" onLoad={gapiLoaded} onError={e => console.log(e)} ></Script>
-      <Script async defer src="https://accounts.google.com/gsi/client" onLoad={gisLoaded} onError={e => console.log(e)} ></Script>
-    </>
-  )
+    </div>
+    <Script async defer src="https://apis.google.com/js/api.js" onLoad={gapiLoaded} onError={e => console.log(e)} ></Script>
+    <Script async defer src="https://accounts.google.com/gsi/client" onLoad={gisLoaded} onError={e => console.log(e)} ></Script>
+  </>)
 }
